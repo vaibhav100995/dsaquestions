9ae4f61a420a2fd70614024ec1ecd5b7b3d0f3b9
vaibhav100995 <vaibhav100995@gmail.com>
2023-02-12 19:22:58 +0530

    longest uncommon subsequence 2 solution - important

---
+++ src/dsaquestions/LongestUncommonSubsequenceII.java
@@ -0,0 +1,62 @@
+package dsaquestions;
+
+
+//Given an array of strings strs, return the length of the longest uncommon subsequence between them. If the longest uncommon subsequence does not exist, return -1.
+//
+//An uncommon subsequence between an array of strings is a string that is a subsequence of one string but not the others.
+//
+//A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+//
+//For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+// 
+//
+//Example 1:
+//
+//Input: strs = ["aba","cdc","eae"]
+//Output: 3
+//Example 2:
+//
+//Input: strs = ["aaa","aaa","aa"]
+//Output: -1
+// 
+//
+//Constraints:
+//
+//2 <= strs.length <= 50
+//1 <= strs[i].length <= 10
+//strs[i] consists of lowercase English letters.
+
+
+
+public class LongestUncommonSubsequenceII {
+
+	public static void main(String[] args) {
+		String[] strs = {"aabbcc","aabbcc","d"};
+		System.out.println(findLUSlength(strs));
+	}
+	
+	public static int findLUSlength(String[] strs) {
+        int res=-1, n=strs.length;
+        for (int i=0; i<n; i++){
+        	int j = 0;
+        	while(j < n) {
+        	    if (i != j && isSubsequence(strs[i], strs[j]))
+        	        break;
+        	    j++;
+        	}
+            if (j==n) {
+            	res=Math.max(res, strs[i].length());
+            }
+        }
+        return res;
+    }
+	
+    public static boolean isSubsequence(String a, String b){
+        int i=0, j=0;
+        while(i<a.length() && j<b.length()) {
+        	if (a.charAt(i)==b.charAt(j++)) i++;
+        }
+        return i==a.length();
+    }
+
+}
