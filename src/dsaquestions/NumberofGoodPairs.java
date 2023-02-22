000d8a8ab3c13ec2fac839276679fbab0efc3276
vaibhav100995 <vaibhav100995@gmail.com>
2023-02-22 22:01:29 +0530

    1512. Number of Good Pairs

---
+++ src/dsaquestions/NumberofGoodPairs.java
@@ -0,0 +1,58 @@
+package dsaquestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Given an array of integers nums, return the number of good pairs.
+//
+//A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [1,2,3,1,1,3]
+//Output: 4
+//Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+//Example 2:
+//
+//Input: nums = [1,1,1,1]
+//Output: 6
+//Explanation: Each pair in the array are good.
+//Example 3:
+//
+//Input: nums = [1,2,3]
+//Output: 0
+// 
+//
+//Constraints:
+//
+//1 <= nums.length <= 100
+//1 <= nums[i] <= 100
+
+public class NumberofGoodPairs {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,1,1,3};
+		System.out.println(numIdenticalPairs(nums));
+	}
+	
+	public static int numIdenticalPairs(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(nums[0],1);
+        for(int i=1;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+
+        int ans=0;
+        for(Integer value:map.values()) {
+        	ans=ans+((value*(value+1))/2-value);
+        }
+        return ans;
+    }
+
+}
