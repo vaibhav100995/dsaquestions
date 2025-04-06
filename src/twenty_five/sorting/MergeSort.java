565edb1b4efe37b486291e8ab65e686bf6e873c9
vaibhav100995 <vaibhav100995@gmail.com>
2025-04-06 18:01:17 +0530

    Sorting Algos (Merge)

---
+++ src/twenty_five/sorting/MergeSort.java
@@ -0,0 +1,64 @@
+package twenty_five.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int arr[] = {4,1,3,9,7};
+        mergeSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*
+    * first pick mid of the array
+    * then call below recursive function first for low->mid, then mid+1->high
+    * at end call merge function to join low to high
+    * */
+    private static void mergeSort(int arr[], int l, int r) {
+        // code here
+        if(l>=r) return;
+        int mid=(l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+
+    /*
+    * left point to first left index of array(l)
+    * right point to first right index of array(mid+1)
+    * compare left index value with right index, whichever is smaller add in temp array and increase index of smaller element
+    *
+    * at end iterate on temp array from l->r and
+    * insert temp array value into real arr array
+    * */
+    private static void merge(int[] arr, int l, int mid, int r){
+        int temp[] = new int[r-l+1];
+        int left=l;
+        int right=mid+1;
+        int i=0;
+        while(left<=mid && right<=r){
+            if(arr[left]>arr[right]){
+                temp[i]=arr[right];
+                right++;
+            }else{
+                temp[i]=arr[left];
+                left++;
+            }
+            i++;
+        }
+        while(left<=mid){
+            temp[i]=arr[left];
+            left++;
+            i++;
+        }
+        while(right<=r){
+            temp[i]=arr[right];
+            right++;
+            i++;
+        }
+        for(int j=l;j<=r;j++){
+            arr[j]=temp[j-l];
+        }
+    }
+}
